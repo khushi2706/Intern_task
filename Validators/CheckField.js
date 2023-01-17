c8bc70d6a613113010579b53a526fe6f487927a3
Khushi patel <67452985+khushi2706@users.noreply.github.com>
2023-01-17 15:37:15 +0530

    Check for require field done

---
+++ Validators/CheckField.js
@@ -0,0 +1,41 @@
+import { CheckType } from "./typeValidator.js"
+
+const objSchema = {
+  name: {
+    type: "string",
+  },
+  email: {
+    type: "string",
+    require: true,
+  },
+  password: {
+    type: "string",
+    require: true,
+  },
+  age: {
+    type: "number",
+  },
+}
+
+function checkField(obj) {
+  let finalAns = true // variable to check the all field types
+
+  // itrate the schema
+  for (let key in objSchema) {
+    // if require object
+    if (objSchema[key].require && obj[key] == undefined) {
+      return false
+    }
+    if (obj[key])
+      finalAns = finalAns && CheckType(obj[key], objSchema[key].type)
+  }
+  return finalAns
+}
+
+let obj = {
+  name: "khushi",
+  email: "khushi@mail.com",
+  password: "abc",
+}
+
+console.log(checkField(obj))
