44b2f734db80df11d84a2dce3aed0019f2ec68a6
Khushi patel <67452985+khushi2706@users.noreply.github.com>
2023-01-18 17:43:45 +0530

    added dummy data

--- Mongodb-practice/index.js
+++ Mongodb-practice/index.js
@@ -1,4 +1,16 @@
 const express = require("express")
 const app = express()
+const { Account } = require("./model/account")
+const { Client } = require("./model/client")
+const { User } = require("./model/users")
+const mongoose = require("mongoose")
+mongoose
+  .connect("mongodb://localhost:27017/MongoPrac")
+  .then(() => {
+    console.log("connected!")
+  })
+  .catch((err) => {
+    console.log("error")
+  })
 
 app.listen(5000)
