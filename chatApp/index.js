b4021e534273d61543312434f2efbfd7cfd335c4
Khushi patel <67452985+khushi2706@users.noreply.github.com>
2023-02-05 13:57:46 +0530

    added envirourmental variable

--- chatApp/index.js
+++ chatApp/index.js
@@ -13,6 +13,7 @@ const {
   getMessageInPrivate,
   storeInPrivateChat,
 } = require("./model/privateMessage")
+const { getListOfClientId, addNewClientId } = require("./utils/connectRedis")
 // redis
 
 const app = new Koa()
@@ -23,20 +24,11 @@ koaEjs(app, {
 })
 const server = http.createServer(app.callback())
 const io = socketio(server)
-const clients = new Map()
 
 app.use(bodyParser()).use(router.routes()).use(router.allowedMethods())
 
-function sendMessage(socket) {
-  redis.lrange("message", "0", "-1", (err, data) => {
-    console.log(data)
-  })
-}
-
 io.on("connection", (socket) => {
   socket.on("joined", async ({ username, room, time }) => {
-    console.log(username, room, time)
-    if (!clients.has(username)) clients.set(username, socket.id)
     socket.join(room)
     let msg = { message: `${username} has joined`, from: "system", time }
 
@@ -46,46 +38,39 @@ io.on("connection", (socket) => {
   })
 
   socket.on("joinPrivate", async ({ username, recipient }) => {
-    if (!clients.has(username)) clients.set(username, [socket.id])
-    else clients.get(username).push(socket.id)
+    addNewClientId(username, socket.id)
     const msgHistory = await getMessageInPrivate(recipient, username)
     socket.emit("sendHistory", msgHistory)
   })
 
   socket.on("sendPrivate", async ({ username, recipient, message }) => {
-    const resIds = clients.get(recipient)
-    const userIds = clients.get(username)
+    const resIds = await getListOfClientId(recipient)
+    const userIds = await getListOfClientId(username)
 
     await storeInPrivateChat(username, message, recipient)
 
     resIds &&
       resIds.forEach((resId) => {
-        console.log(resId)
         const resSocket = io.sockets.sockets.get(resId)
+        // socketId : socket
         resSocket &&
           resSocket.emit("message", {
             message,
             from: username,
-            time: new Date(),
           })
       })
     userIds &&
       userIds.forEach((resId) => {
-        console.log(resId)
         const resSocket = io.sockets.sockets.get(resId)
         resSocket &&
           resSocket.emit("message", {
             message,
             from: username,
-            time: new Date(),
           })
       })
   })
 
   socket.on("sendmessage", ({ message, from, room, time }) => {
-    console.log(message, from, time)
-    //   redis.rpush("message", `${from}:${message}`)
-
     storeInRoom(room, message, from)
     socket.in(room).emit("message", { message, from, time })
   })
--- chatApp/utils/connectRedis.js
+++ chatApp/utils/connectRedis.js
@@ -1,6 +1,31 @@
 const Redis = require("ioredis")
 const redis = new Redis({
-  host: "redis-15439.c305.ap-south-1-1.ec2.cloud.redislabs.com",
+  host: process.env.URL_REDIS,
   port: 15439,
-  password: "UWLWItgjTemAoyETSH57YxTK36p8ig1y",
+  password: process.env.PASSWORD_REDIS,
 })
+
+const addNewClientId = (username, socketId) => {
+  redis.exists(username, async (err, reply) => {
+    if (reply == 1) {
+      const valueOfArr = (await getListOfClientId(username)) || []
+      console.log(valueOfArr)
+      valueOfArr.push(socketId)
+      await redis.hset(username, "Ids", JSON.stringify(valueOfArr))
+    } else {
+      let arr = [socketId]
+      const res = await redis.hset(username, "Ids", JSON.stringify(arr))
+    }
+  })
+}
+
+const getListOfClientId = async (username) => {
+  console.log(username)
+  const value = await redis.hget(username, "Ids", (err, value) => {
+    console.log(JSON.parse(value))
+    return JSON.parse(value)
+  })
+  return JSON.parse(value)
+}
+
+module.exports = { addNewClientId, getListOfClientId }
